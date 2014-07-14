e4a6bba107a0dc158b34f082456b7949cd8518f8
Austin Steward & Peggy Condon <student@epicodus.com>
2014-07-14 15:24:40 -0700

    Added relationships to address book

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,70 @@
 $(document).ready(function() {
+  $('#add-address').click(function(){
+      $("#new-addresses").append('<hr /><br /><div class="new-address">'+
+                                  '<div class="form-group">' +
+                                  '<label for="new-street">Street</label>' +
+                                  '<input type="text" class="form-control new-street">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                  '<label for="new-city">City</label>' +
+                                  '<input type="text" class="form-control new-city">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                  '<label for="new-state">State</label>' +
+                                  '<input type="text" class="form-control new-state">' +
+                                  '</div>');
+  });
+
   $("form#new-contact-form").submit(function(event) {
     event.preventDefault();
     var newFirstName = $("input#new-first-name").val();
     var newLastName = $("input#new-last-name").val();
-    var newAddress = $("input#new-address").val();
-    var newContact = {firstName: newFirstName, lastName: newLastName, address: newAddress};
+    if((newFirstName === "") || (newLastName === "")){
+      alert("Please enter a valid first AND last name");
+    } else {
+      var newContact = {firstName: newFirstName, lastName: newLastName, addresses: []};
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-address").val("");
+      $(".new-address").each(function() {
+        var newStreet = $(this).find("input.new-street").val();
+        var newCity = $(this).find("input.new-city").val();
+        var newState = $(this).find("input.new-state").val();
 
-    $("ul#contact-name").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
-    $(".contact-list").show();
+        if(newStreet === "") {
+          newStreet = "Unknown";
+        }
+        if(newCity === "") {
+          newCity = "Unknown";
+        }
+        if(newState === "") {
+          newState = "Unknown";
+        }
 
-    $(".contact").last().click(function() {
-      $("#contactDetails h2").text(newContact.firstName + " " + newContact.lastName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
-      $("#contactDetails").show();
-    });
+        var newAddress = {street: newStreet, city: newCity, state: newState};
+        newContact.addresses.push(newAddress);
+      });
 
+
+      $("ul#contact-name").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+      $(".contact-list").show();
+
+      $(".contact").last().click(function() {
+        $("#contactDetails h2").text(newContact.firstName + " " + newContact.lastName);
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+        $("dl#addresses").text("");
+        $("#contactDetails").show();
+
+        newContact.addresses.forEach(function(address) {
+          $("dl#addresses").append("<dd>Street: " + address.street + "</dd><dd>City: " + address.city + "</dd><dd>State: " + address.state + "</dd><hr />");
+        });
+      });
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input.new-street").val("");
+      $("input.new-city").val("");
+      $("input.new-state").val("");
+    } //End if statement
   });
-
-
 });
 
